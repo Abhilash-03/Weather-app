7af1789ca10f01d8ec8f16fa323f7eab02b16a46
Abhilash <abhilashjha335@gmail.com>
2023-02-25 16:16:24 +0530

    change in design and responsive

--- app.js
+++ app.js
@@ -26,19 +26,20 @@ const showData = async (data) => {
   try {
     let curr = data["current"];
     let loc = data["location"];
-    // let air = curr["air_quality"]
     let showCard = "";
+
     showCard += `
-    <div class="card-header text-info ">Country:
-    <b><span id="country" class="text-warning">${loc.country} </span></b> Weather
-    <p class="text-warning ">Region: <span class="text-danger">${loc.region}</span></p>
+    <div class="card-header text-danger " id="place">
+    <span id="country">Country:</span>
+    <b><span id="countryName" class="text-warning">${loc.country} </span></b> 
+    <p class="text-warning " id="region">Region: <span class="text-danger">${loc.region}</span></p>
 </div>
-<div class="img container" id="img">
+<div class="img container-fluid" id="img">
 <img src=${curr.condition.icon} class="img-fluid img-responsive  img-lg" alt="icon">
 </div>
 <div class="card-body " id="cardBody">
-    <h1 class="card-title  pb-4">Temperature</h1>
-    <div class='row'>
+    <h1 class="card-title temp pb-4">Temperature</h1>
+    <div class='row' id="rowCol">
     <div class='col-md-4'>
     <h3 class="card-title text-bg-info ">Celcius: <span id="temp_c">${curr.temp_c} °C</span></h3>
     <h4 class="card-title ">Fahrenheit : <span id="temp_f " class="text-danger">${curr.temp_f} °F</span></h5>
@@ -46,10 +47,10 @@ const showData = async (data) => {
     <div class='col-md-4'>
 
     <h3 class="card-title text-bg-danger">Humidity : <span id="humidity">${curr.humidity}</span></h3>
-    <h4 class="card-title">Ultra violate : <span id="uv">${curr.uv}</span></h4>
+    <h4 class="card-title text-warning">Ultra violate : <span id="uv">${curr.uv}</span></h4>
     </div>
     <div class='col-md-4'>
-    <h3 class="card-title text-bg-primary">Latitude : <span id="lat">${loc.lat}</span></h3>
+    <h3 class="card-title text-bg-info">Latitude : <span id="lat">${loc.lat}</span></h3>
     <h4 class="card-title">Longitude : <span id="lon">${loc.lon}</span></h4>
    </div>
    </div>
@@ -57,16 +58,8 @@ const showData = async (data) => {
 </div>
     `;
     weatherCard.innerHTML = showCard;
-    let arr = [
-      "temp_c",
-      "temp_f",
-      "feelslike_c",
-      "feelslike_f",
-      "uv",
-      "humidity",
-      "wind_kph",
-      "cloud",
-    ];
+
+    let arr = ["temp_c","temp_f","feelslike_c","feelslike_f","uv","humidity","wind_kph","cloud",];
 
     // Displaying weather info. in table.
     Array.from(td).map((e, index) => (e.innerHTML = curr[`${arr[index]}`]));
